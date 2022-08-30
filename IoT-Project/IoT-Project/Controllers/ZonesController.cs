a666d540b4c45103da50506c8a35d0f231c1e5da
Dennis van Tonder <dennisvantonder@outlook.com>
2022-08-30 14:15:54 +0200

    Added security to API

--- IoT-Project/IoT-Project/Controllers/ZonesController.cs
+++ IoT-Project/IoT-Project/Controllers/ZonesController.cs
@@ -6,9 +6,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using IoT_Project.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace IoT_Project.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ZonesController : ControllerBase
